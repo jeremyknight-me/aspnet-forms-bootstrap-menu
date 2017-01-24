3cdbc1fce2bbdfaf6d3772bdd3888bc049981232
knight0323 <jeremy@theknights.me>
2017-01-23 21:39:47 -0600

    initial check in

---
+++ ExampleWithDll/Default.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ExampleWithDll.Default" %>
+<%@ Register TagPrefix="jk" Namespace="JK.BootstrapControls" Assembly="JK.BootstrapControls" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title>ASP.NET Forms Bootstrap Menu Example</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+               
+        <h3>Bootstrap</h3>
+        
+        <div id="main-nav" class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container" style="padding: 0; margin: 0;">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+        
+        <h3>BootstrapMenu ASP.NET Control</h3>
+        
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container" style="padding: 0; margin: 0;">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <jk:BootstrapMenu ID="BootstrapMenu1" runat="server" HighlightActive="True">
+                        <Items>
+                            <asp:MenuItem Text="Link" NavigateUrl="#" />
+                            <asp:MenuItem Text="Link" NavigateUrl="#" />
+                            <asp:MenuItem Text="Drop Down">
+                                <asp:MenuItem Text="Link" NavigateUrl="#" />
+                                <asp:MenuItem Text="Link" NavigateUrl="#" />
+                                <asp:MenuItem Text="Link" NavigateUrl="#" />
+                            </asp:MenuItem>
+                            <asp:MenuItem Text="Link" NavigateUrl="#" />
+                            <asp:MenuItem Text="Nothing" />
+                        </Items>
+                    </jk:BootstrapMenu>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+
+        <h3>BootstrapMenu ASP.NET Control SiteMap</h3>
+        
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+            <div class="container" style="padding: 0; margin: 0;">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <jk:BootstrapMenu ID="BootstrapMenu2" runat="server" DataSourceId="SiteMapDataSource1" />
+                    <asp:SiteMapDataSource ID="SiteMapDataSource1" runat="server" ShowStartingNode="False" />
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+    </div>
+    </form>
+    
+    <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ JK.BootstrapControls/BootstrapMenu.cs
@@ -0,0 +1,215 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BootstrapMenu.cs">
+//     Copyright (c) Jeremy Knight. All rights reserved.
+//     This source is subject to The MIT License (MIT).
+//     For more information, see https://github.com/knight0323/aspnet-forms-bootstrap-menu
+// </copyright>
+// <author>Jeremy Knight</author>
+//-----------------------------------------------------------------------
+
+using System;
+using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JK.BootstrapControls
+{
+    [ControlValueProperty("SelectedValue")]
+    [DefaultEvent("MenuItemClick")]
+    [SupportsEventValidation]
+    [ToolboxData("<{0}:BootstrapMenu runat=\"server\"></{0}:BootstrapMenu>")]
+    public sealed class BootstrapMenu : Menu
+    {
+        private const string hightlightActiveKey = "HighlightActive";
+
+        /// <summary>
+        /// Gets or sets the header text over the left list box.
+        /// </summary>
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [DefaultValue(false)]
+        [DisplayName("HighlightActive")]
+        public bool HighlightActive
+        {
+            get { return this.ViewState[hightlightActiveKey] != null && Convert.ToBoolean(this.ViewState[hightlightActiveKey]); }
+            set { this.ViewState[hightlightActiveKey] = value; }
+        }
+
+        public override void RenderBeginTag(HtmlTextWriter writer)
+        {
+            // don't call base.RenderBeginTag()
+        }
+
+        public override void RenderEndTag(HtmlTextWriter writer)
+        {
+            // don't call base.RenderEndTag()
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            // don't call base.OnPreRender(e);
+            this.EnsureDataBound();
+        }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            this.BuildItems(writer, this.Items, true);
+        }
+
+        protected override void EnsureDataBound()
+        {
+            base.EnsureDataBound();
+        }
+
+        private void BuildItems(HtmlTextWriter writer, MenuItemCollection items, bool isRoot = false)
+        {
+            if (items.Count <= 0)
+            {
+                return;
+            }
+
+            string cssClass = "dropdown-menu";
+
+            if (isRoot)
+            {
+                cssClass = "nav navbar-nav";
+                if (!string.IsNullOrEmpty(this.CssClass))
+                {
+                    cssClass += " " + this.CssClass;
+                }
+            }
+
+            writer.AddAttribute(HtmlTextWriterAttribute.Class, cssClass);
+            writer.RenderBeginTag(HtmlTextWriterTag.Ul);
+
+            foreach (MenuItem item in items)
+            {
+                this.BuildItem(writer, item);
+            }
+
+            writer.RenderEndTag(); // </ul>
+        }
+
+        private void BuildItem(HtmlTextWriter writer, MenuItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            if (item.ChildItems.Count > 0)
+            {
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "dropdown");    
+            }
+            
+            if (this.IsLink(item))
+            {
+                if (this.HighlightActive && this.Page.ResolveUrl(item.NavigateUrl) == this.Page.Request.Url.AbsolutePath)
+                {
+                    writer.AddAttribute("class", "active");
+                }
+
+                writer.RenderBeginTag(HtmlTextWriterTag.Li);
+                this.RenderLink(writer, item);
+            }
+            else if (this.HasChildren(item))
+            {
+                writer.RenderBeginTag(HtmlTextWriterTag.Li);
+                this.RenderDropDown(writer, item);
+            }
+            else
+            {
+                writer.RenderBeginTag(HtmlTextWriterTag.Li);
+                writer.RenderBeginTag(HtmlTextWriterTag.A);
+                writer.Write(item.Text);
+                writer.RenderEndTag();
+            }
+
+            writer.RenderEndTag(); // </li>
+        }
+
+        private bool HasChildren(MenuItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            return item.ChildItems.Count > 0;
+        }
+
+        private bool IsLink(MenuItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            return item.Enabled && !string.IsNullOrEmpty(item.NavigateUrl);
+        }
+
+        private void RenderLink(HtmlTextWriter writer, MenuItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            string href = !string.IsNullOrEmpty(item.NavigateUrl)
+                    ? this.Page.Server.HtmlEncode(this.ResolveClientUrl(item.NavigateUrl))
+                    : this.Page.ClientScript.GetPostBackClientHyperlink(
+                        this,
+                        "b" + item.ValuePath.Replace(this.PathSeparator.ToString(), "\\"),
+                        true);
+            writer.AddAttribute(HtmlTextWriterAttribute.Href, href);
+
+            string toolTip = !string.IsNullOrEmpty(item.ToolTip)
+                ? item.ToolTip
+                : item.Text;
+            writer.AddAttribute(HtmlTextWriterAttribute.Title, toolTip);
+
+            writer.RenderBeginTag(HtmlTextWriterTag.A);
+            writer.Write(item.Text);
+            writer.RenderEndTag(); // </a>
+        }
+
+        private void RenderDropDown(HtmlTextWriter writer, MenuItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            writer.AddAttribute(HtmlTextWriterAttribute.Href, "#");
+            writer.AddAttribute(HtmlTextWriterAttribute.Class, "dropdown-toggle");
+            writer.AddAttribute("data-toggle", "dropdown");
+            writer.RenderBeginTag(HtmlTextWriterTag.A);
+
+            string anchorValue = item.Text + "&nbsp;";
+            writer.Write(anchorValue);
+
+            writer.AddAttribute(HtmlTextWriterAttribute.Class, "caret");
+            writer.RenderBeginTag(HtmlTextWriterTag.B);
+            writer.RenderEndTag(); // </b>          
+
+            writer.RenderEndTag(); // </a>
+
+            this.BuildItems(writer, item.ChildItems);
+        }
+    }
+}
